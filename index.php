7d0b265c39bc8feedcf9249e78b3bd52981adc83
Sebastian Nilsson <sebnil@gmail.com>
2014-01-06 21:22:12 +0100

    general code improvements

--- index.php
+++ index.php
@@ -1,27 +1,53 @@
 <?php
-ini_set('error_reporting', 'E_ALL');
+ini_set('error_reporting', E_ALL);
 ini_set('display_errors', 'On');
 
-$url = "http://www.yr.no/sted/Sverige/V%C3%A4stra_G%C3%B6taland/G%C3%B6teborg/varsel_time_for_time.xml";
 
+
+// read some variables from url
+if (!empty($_GET["debug"]))
+    $debug = true;
+else
+    $debug = false;
+if (!empty($_GET["threshold"]))
+    $precipitationThreshold = $_GET["threshold"];
+else
+    $precipitationThreshold = 0;
+
+if (!empty($_GET["yr_url"]))
+    $url = $_GET["yr_url"].'varsel_time_for_time.xml';
+else
+    $url = "http://www.yr.no/sted/Sverige/V%C3%A4stra_G%C3%B6taland/G%C3%B6teborg/varsel_time_for_time.xml";    
+
+// get forecast
 $forecastXml = new SimpleXMLElement(curl_download($url));
-$times = $forecastXml->forecast->tabular;
+//$times = $forecastXml->forecast->tabular;
 //print_r($forecastXml);
 
+// search for precipitation the next 10 hours
 $rain = false;
+$maxPrecipitation = 0;
+$error = false;
 for ($i = 0; $i<10; $i++) {
-    //print_r($forecastXml->forecast->tabular->time[$i]->precipitation->attributes()->value);   
-    if ((float)$forecastXml->forecast->tabular->time[$i]->precipitation->attributes()->value > 0) {
+    // check if precipitation is above threshold
+    if ((float)$forecastXml->forecast->tabular->time[$i]->precipitation->attributes()->value > $precipitationThreshold) {
         // gonna rain
+        $maxPrecipitation = (float)$forecastXml->forecast->tabular->time[$i]->precipitation->attributes()->value;
         $rain = true;
         break;
     }
 }
-echo "rain: ";
-echo $rain?1:0;
+if ($debug) {
+    echo "url: $url <br/>";
+    echo "precipitationThreshold: $precipitationThreshold <br/>";   
+    echo "maxPrecipitation: $maxPrecipitation <br/>";   
+    echo "error: ".($error?1:0)."<br/>";
+}
+
+echo "rain: ".($rain?1:0)."\n";
 
 
-
+// just return the content of a url
 function curl_download($Url){
  
     // is cURL installed yet?
